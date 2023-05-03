4044075bc5a2a6dd3e6f5ab011ec2fe14edafee7
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-05-03 08:44:44 -0600

    Last iter

---
+++ Home-act-repo 25/Act-1/cmake-build-debug/CMakeFiles/Act_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/david/Documents/GitHub/Estructura-de-datos-homework/Home-act-repo 25/Act-1/main.c" "CMakeFiles/Act_1.dir/main.c.obj" "gcc" "CMakeFiles/Act_1.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Home-act-repo 25/Act-1/cmake-build-debug/CMakeFiles/Act_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Act_1.exe"
+  "Act_1.exe.manifest"
+  "Act_1.pdb"
+  "CMakeFiles/Act_1.dir/main.c.obj"
+  "CMakeFiles/Act_1.dir/main.c.obj.d"
+  "libAct_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Act_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
