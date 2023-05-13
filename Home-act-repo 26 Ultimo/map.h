3a190e0bc76894f4eaf7af34184a4c55a3fce3e4
David Hernández <david.hernandezc@iteso.mx>
2023-05-12 18:13:55 -0600

    requisitos

--- Home-act-repo 26 Ultimo/map.h
+++ Home-act-repo 26 Ultimo/map.h
@@ -4,5 +4,26 @@
 
 #ifndef HOME_ACT_REPO_26_ULTIMO_MAP_H
 #define HOME_ACT_REPO_26_ULTIMO_MAP_H
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+struct MAP {
+    char key[60];
+    char data[80];
+    struct MAP *next;
+};
+
+typedef struct LIST MAPLIST;
+unsigned long long hash(char *s);
+void maplist_table_init(MAPLIST *table,int m);
+void maplist_insert(MAPLIST *list,char *key,char *data);
+void maplist_display(MAPLIST list);
+void map_insert(MAPLIST *map,char *key,char *data);
+int map_get(struct MAP m,TYPE key);
+void map_destroy(struct MAP m);
 
 #endif //HOME_ACT_REPO_26_ULTIMO_MAP_H
--- Home-act-repo 26 Ultimo/programa.c
+++ Home-act-repo 26 Ultimo/programa.c
@@ -1,7 +1,3 @@
 //
 // Created by David on 12/05/2023.
 //
-
-#include "map.h"
-
-
